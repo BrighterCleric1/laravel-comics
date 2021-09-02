b8ed1e9f82698a72fd7e2d5e29ccdb9ce0a600b9
BrighterCleric1 <simontemplar99@hotmail.it>
2021-09-02 16:55:51 +0200

    first page complete

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,3 +1,88 @@
 <div>
-    pippo
+<div class="footer-cont">
+    <div class="first-f">
+      <div class="container">
+        <div class="row justify-content-between">
+          <div class="col">
+            <ul>
+              <li>DC COMICS</li>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">Tv</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">News</a></li>
+            </ul>
+            <ul>
+              <li>SHOP</li>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop Dc Collectibles</a></li>
+            </ul>
+          </div>
+          <div class="col">
+            <ul>
+              <li>DC</li>
+              <li><a href="#">Termy Of Use</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+              <li><a href="#">Ad Choices</a></li>
+              <li><a href="#">Advertising</a></li>
+              <li><a href="#">Jobs</a></li>
+              <li><a href="#">Subrscriptions</a></li>
+              <li><a href="#">Talent Workshops</a></li>
+              <li><a href="#">Ratings</a></li>
+              <li><a href="#">Shop Help</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col">
+            <ul>
+              <li>SITES</li>
+              <li><a href="#">DC</a></li>
+              <li><a href="#">MAD Magazines</a></li>
+              <li><a href="#">DC Kids</a></li>
+              <li><a href="#">DC Universe</a></li>
+              <li><a href="#">DC Power Visa</a></li>
+            </ul>
+          </div>
+          <div class="col col-logo">
+            <div class="img-footer">
+              <img src="/images/dc-logo-bg.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="second-f">
+      <div class="container">
+        <div class="row justify-content-between align-items-center">
+          <div class="col">
+            <div class="button-sign">
+              <button class="signup">SIGN-UP NOW!</button>
+            </div>
+          </div>
+          <div class="col">
+            <ul class="social d-flex align-items-center">
+              <li>FOLLOW US</li>
+              <li>
+                <img src="/images/footer-facebook.png" alt="" />
+              </li>
+              <li>
+                <img src="/images/footer-periscope.png" alt="" />
+              </li>
+              <li>
+                <img src="/images/footer-twitter.png" alt="" />
+              </li>
+              <li>
+                <img src="/images/footer-pinterest.png" alt="" />
+              </li>
+              <li>
+                <img src="/images/footer-youtube.png" alt="" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
