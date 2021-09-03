77b3b8f71beaaef2c4b8b0fccb4a2389a3de7afd
BrighterCleric1 <simontemplar99@hotmail.it>
2021-09-03 16:38:12 +0200

    imgs errors

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,36 +4,39 @@
 
     <div class="row-blue-2"></div>
     <div class="container">
-        <div class="row">
+        <div class="row row-product">
             <div class="offset-2 col-7">
                 <div class="comic-title">{{ $comics[$arrayIndex]['title'] }}</div>
                 <div class="buy-avb row">
-                    <div class="col-9 d-flex justify-content-between">
+                    <div class="col-9 align-items-center d-flex justify-content-between green-button">
                         <div>
                             <span>U.S. Price: </span>
-                            <span>{{ $comics[$arrayIndex]['price'] }}</span>
+                            <span><b>{{ $comics[$arrayIndex]['price'] }}</b></span>
                         </div>
-                        <div>asd</div>
+                        <div>AVAILABLE</div>
                     </div>
-                    <div class="col-3">
+                    <div class="col-3 d-flex green-button border-l align-items-center">
                         <div>
-                            <span>Check Availability</span>
+                            <span><b>Check Availability</b></span>
                         </div>
                     </div>
                 </div>
-                <div>{{ $comics[$arrayIndex]['description'] }}</div>
+                <div class="description">{{ $comics[$arrayIndex]['description'] }}</div>
             </div>
             <div class="col-3">
-                <div class="advertisement">
-                    <div class="inner-ad">
-                        <div class="ad-img">
-                            <img src="/images/adv.jpg" alt="">
+                <div >
+                    <div >
+                        <div >
+                            <img src="/images/footer.twitter.png" alt="">
                         </div>
                     </div>
+                    <span>cacsacaca</span>
                 </div>
             </div>
         </div>
 
     </div>
+    <img src="/images/footer-facebook.png" alt="">
+    <img src="/images/footer.facebook.png" alt="">
 
 @endsection
